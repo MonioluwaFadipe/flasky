4d94fa5577d082fc87b677f0b1f6ffe077c9abfe
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-05-24 14:25:11 +0100

    Add webforms

--- hello.py
+++ hello.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY']= 'You cannot see me'
 bootstrap= Bootstrap(app)
 moment = Moment(app)
 
@@ -16,6 +20,10 @@ def index():
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 """
 @app.error_handler(404)
