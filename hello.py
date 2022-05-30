c935dd21be3664012c62275abe9ee439864d45b5
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-05-30 19:25:44 +0100

    Fix dirname typo

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ import os
 from flask_sqlalchemy import SQLAlchemy
 
 
-basedir = os.path.abspath(os.path.dirmap(__file__))
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
