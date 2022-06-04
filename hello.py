ef1e48abc021720521976a3060d8752a9aa66a5a
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-06-04 16:59:38 +0100

    Create an email example

--- hello.py
+++ hello.py
@@ -12,6 +12,7 @@ from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_mail import Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -23,6 +24,9 @@ app.config['MAIL_PORT'] = '587'
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flask Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
 bootstrap= Bootstrap(app)
 moment = Moment(app)
 
@@ -41,6 +45,9 @@ def index():
             db.session.add(user)
             db.session.commit()
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'], 'New User', 
+                'mail/new_user', user=user)
         else:
             session['known'] = True
         session['name'] = form.name.data
@@ -79,3 +86,10 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject,
+                sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
