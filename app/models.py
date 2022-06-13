fe65166efb43597f46699d6269775ce2949e7470
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-06-13 18:46:36 +0100

    Add user loader function

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from . import login_manager
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -35,4 +36,6 @@ class User(UserMixin, db.Model):
         return '<User %r>' % self.username
 
     
-
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
