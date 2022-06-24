673e7ea4a7d361db97b8a1a038b4dda806b52b36
Fadipe Daniel <fadipemonioluwa@gmail.com>
2022-06-24 21:20:51 +0100

    Register new user

--- app/auth/views.py
+++ app/auth/views.py
@@ -4,7 +4,7 @@ from flask_login import login_user
 from app import email
 from . import auth
 from ..models import User 
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask_login import logout_user, login_required
 
 @auth.route('/login', methods=['GET', 'POST'])
@@ -27,3 +27,14 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main_index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data, username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can now login')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=form)
